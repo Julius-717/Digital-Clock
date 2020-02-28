7530b981b0b030335edd27ca015c3c3ca8a02942
Julius-717 <waynejulius10@gmail.com>
2020-02-29 01:30:49 +0300

    add function to update time

--- main.js
+++ main.js
@@ -33,4 +33,34 @@ $(function() {
     function setPref(name, value) {
         if(store) store.setItem(name, value);
     }
+
+    function updateTime() {
+        var now, h, m, s, nowstr, tz, hours, ap;
+
+        separator.toggleClass('on');
+
+        now = new Date();
+        h = now.getHours();
+        m = now.getMinutes();
+        s = now.getSeconds();
+        nowstr = now.toString();
+        tz = (nowstr.match(/\b([A-Z]{1,4}).$/) || ['']).pop();
+        hours = getPref('hr', 12);
+        ap = (h >= hours) ? 'pm' : 'am';
+
+        clock.addClass('tz-' + tz.toLowerCase());
+
+        if(hours == 12) {
+            h = (h === 0) ? 12 : (h > 12) ? h % 12 : h;
+            ampm.removeClass('am pm').addClass(ap);
+        }
+
+        digits.removeClass('d0 d1 d2 d3 d4 d5 d6 d7 d8 d9')
+		.eq(0).addClass('d' + fl(h / 10)).end()
+		.eq(1).addClass('d' + (h % 10)).end()
+		.eq(2).addClass('d' + fl(m / 10)).end()
+		.eq(3).addClass('d' + (m % 10)).end()
+		.eq(4).addClass('d' + fl(s / 10)).end()
+		.eq(5).addClass('d' + (s % 10));
+    }
 })
